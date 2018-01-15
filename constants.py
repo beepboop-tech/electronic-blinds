2395cca9376e86b7d3ac3b05ff8a8e9938ce8faf
tpbadger <tombadger12@gmail.com>
2018-01-15 17:21:34 +0000

    added constants file

---
+++ constants.py
@@ -0,0 +1,11 @@
+M1dirpin = 4  # set motor 1 direction pin
+M1steppin = 5  # set motor 1 step pin
+M2dirpin = 7  # set motor 2 direction pin
+M2steppin = 6  # set motor 2 step pin
+
+relaypin = 21  # set pi GPIO pin for relay
+
+delayStep = 1 / 1000000  # delay in stepper rotation in microseconds
+delayRelay = 0.5  # delay when turning on stepper board
+
+rotations = 2001  # number of steps to fully open/close blinds
