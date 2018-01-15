4ed07a0019d527cc702c53b639c951283e3323e6
Tom Evans <TomosAMEvans@gmail.com>
2018-01-15 21:41:36 +0000

    added the recieve pin

--- motor_control.py
+++ motor_control.py
@@ -1,64 +1,45 @@
-from nanpy import ArduinoApi
-from nanpy import SerialManager
 from time import sleep
 import constants
 import RPi.GPIO as GPIO
 
-connection = SerialManager(device='/dev/ttyACM0')
 
-# slave setup
-a = ArduinoApi(connection=connection)
-a.pinMode(constants.M1dirpin, a.OUTPUT)  # M1 direction pin
-a.pinMode(constants.M1steppin, a.OUTPUT)  # M1 stepping pin
-a.pinMode(constants.M2dirpin, a.OUTPUT)  # M2 direction pin
-a.pinMode(constants.M2steppin, a.OUTPUT)  # M2 stepping pin
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(constants.relaypin, GPIO.OUT)
 
+GPIO.setup(constants.relaypin,     GPIO.OUT)
+GPIO.setup(constants.up_pin,       GPIO.OUT)
+GPIO.setup(constants.up_pin,       GPIO.OUT)
+GPIO.setup(constants.recieved_pin, GPIO.IN)
+
+# The last recieved_pin value
+# Initally False
+current_recieved_pin_value = GPIO.LOW
+
+def set_pin_and_wait(pin_number):
+    GPIO.output(pin_number, GPIO.HIGH)
+
+    while(GPIO.input(constants.recieved_pin) is current_recieved_pin_value):
+        # Wait for the `recieved_pin` to be toggled
+        pass
+
+    GPIO.output(pin_number, GPIO.LOW)
+    current_recieved_pin_value = GPIO.input(constants.recieved_pin)
 
 def turn_stepper_on():
     GPIO.output(constants.relaypin, GPIO.HIGH)
-    sleep(constants.delayRelay)
-
+    sleep(constants.relay_delay)
 
 def turn_stepper_off():
     GPIO.output(constants.relaypin, GPIO.LOW)
-    sleep(constants.delayRelay)
-
+    sleep(constants.relay_delay)
 
 def blinds_up():
-
-    global a
-    
     turn_stepper_on()
-    a.digitalWrite(constants.M1dirpin, a.HIGH)
-    a.digitalWrite(constants.M2dirpin, a.HIGH)
-    for i in range(0, constants.rotations):
-        a.digitalWrite(constants.M1steppin, a.HIGH)
-        a.digitalWrite(constants.M2steppin, a.HIGH)
-        sleep(constants.delayStep)
-        a.digitalWrite(constants.M1steppin, a.LOW)
-        a.digitalWrite(constants.M2steppin, a.LOW)
-        sleep(2 * constants.delayStep)
+    set_pin_and_wait(constants.up_pin)
     turn_stepper_off()
-
 
 def blinds_down():
-
-    global a
-    
     turn_stepper_on()
-    a.digitalWrite(constants.M1dirpin, a.LOW)
-    a.digitalWrite(constants.M2dirpin, a.LOW)
-    for i in range(0, constants.rotations):
-        a.digitalWrite(constants.M1steppin, a.HIGH)
-        a.digitalWrite(constants.M2steppin, a.HIGH)
-        sleep(constants.delayStep)
-        a.digitalWrite(constants.M1steppin, a.LOW)
-        a.digitalWrite(constants.M2steppin, a.LOW)
-        sleep(2 * constants.delayStep)
+    set_pin_and_wait(constants.down_pin)
     turn_stepper_off()
-
-#blinds_up(a)
